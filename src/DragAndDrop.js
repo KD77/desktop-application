95bc5685f02c1863fd5c57ced0ef8edbdced70ea
KD77 <kalid.diriye@gmail.com>
2021-01-10 01:28:29 +0100

    Added contstructor, moveDown, moveUp and move methods to DragAndDrop class

---
+++ src/DragAndDrop.js
@@ -0,0 +1,17 @@
+class DragAndDrop {
+  constructor (window) {
+    this.window = document.querySelector(window)
+
+  }
+moveDown(event) {
+
+}
+moveUp() {
+
+}
+move(event) {
+
+}
+
+}
+export default DragAndDrop
