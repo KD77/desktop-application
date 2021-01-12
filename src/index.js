ce3e208b2a44ad2d91425139a51a9680539f570a
KD77 <kalid.diriye@gmail.com>
2021-01-12 22:48:06 +0100

    Added Index file to start the apps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import DragAndDrop from './DragAndDrop'
+import HangMan from './hangMan'
+
+const hangman = document.getElementById('hngmanGame')
+var hangmanGameID = 1
+hangman.addEventListener('click', function () {
+  const myWindow = new DragAndDrop('.windowContainer')
+  const myChat = new HangMan('.hangManContainer', myWindow, hangmanGameID)
+  hangmanGameID++
+  myWindow.div.appendChild(myChat.div)
+})
 
