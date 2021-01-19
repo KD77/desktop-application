336abe38d04f7adf612ab51147c91398b5d588ee
KD77 <kalid.diriye@gmail.com>
2021-01-19 15:53:18 +0100

    bug fix

--- src/hangMan.js
+++ src/hangMan.js
@@ -9,7 +9,27 @@ export default class HangMan {
     // Array of the secret words
     const fruits = [
       'Apple',
-      'Orange'
+      'Orange',
+      'Plum',
+      'Bananas',
+      'Pears',
+      'Avocado',
+      'Blackberries',
+      'Apricots',
+      'Cantaloupe',
+      'Cherries',
+      'Grapes',
+      'Grapes',
+      'Mandarin',
+      'Mango',
+      'Papaya',
+      'Peaches',
+      'Pineapple',
+      'Raspberries',
+      'Strawberries',
+      'Watermelon',
+      'Blueberries,
+
       
     ]
     // declare & initialize variables
@@ -20,31 +40,31 @@ export default class HangMan {
     this.div = document.importNode(this.hangMan, true)
     this.container.appendChild(this.div)
 
-    this.ID = id
-    this.underscore = []
+    this.ID = id;
+    this.underscoreReplacment = [];
     this.counter = 0
     this.tryCounter = 0
     this.showMessageDiv = this.div.childNodes[1]
+    this.ansText = this.div.childNodes[5]
 
-    this.answer = this.div.childNodes[5]
-    // tittle and window ID
     const divRepresentWindow = document.createElement('div')
-    divRepresentWindow.textContent = 'HangMan' + ' ' + id
+    divRepresentWindow.textContent = 'HangMan' + id
     divRepresentWindow.className = 'representWindow'
-    divRepresentWindow.style.textAlign = 'center'
     myWindow.div.firstElementChild.appendChild(divRepresentWindow)
 
+    //choose one word randomly and convert it to lowerCase
+    var selectWord = fruits[Math.floor(Math.random() * fruits.length)];
+    this.choosenWord = selectWord.toLowerCase();
+    const ptxt = document.createTextNode(this.underScore())
+    const pRepresentWindow = document.createElement('p')
     
-    // randow words
-    var word = fruits[Math.floor(Math.random() * fruits.length)]
-    this.choosenWord = word.toLowerCase()
+    pRepresentWindow.appendChild(ptxt)
+    myWindow.div.appendChild(pRepresentWindow)
 
-    const pRp = document.createElement('p')
-    pRp.style.color = '#000'
-    const ptxts = document.createTextNode(this.underScore())
+    
 
-    // if the user typed a char and pressed enter go to checkChar() to check that char
-    this.answer.addEventListener('keypress', (event) => {
+    //if the user typed a char and pressed enter go to checkChar() to check that char
+    this.ansText.addEventListener('keypress', (event) => {
       document.getElementById('msg').style.display = 'none'
       if (event.keyCode === 13) {
         this.checkChar(myWindow, event.target.value)
@@ -52,55 +72,52 @@ export default class HangMan {
       }
     })
   }
-
-  underScore () {
+  underScore() {
     for (var i = 0; i < this.choosenWord.length; i++) {
-      this.underscore.push(' _ ')
+      this.underscoreReplacment.push(' _ ');
     }
-    return this.underscore
+    return this.underscoreReplacment;
   }
 
-  checkChar (myWindow, chr) {
+  checkChar(myWindow, chr) {
     this.inputValue = chr
-    this.inputValue.trim()
-    this.tryCounter++
-
-    // Loop to check if the this char equals any char in the choosen word
+    this.tryCounter++;
+    //Loop to check if the this char equals any char in the choosen word
     for (var i = 0; i < this.choosenWord.length; i++) {
       if (this.inputValue.charAt(0) === this.choosenWord.charAt(i)) {
-        this.underscore[i] = this.inputValue.charAt(0)
+        this.underscoreReplacment[i] = this.inputValue.charAt(0)
         // if this char equals any char raise the counter by 1
-        this.counter++
-        document.getElementById('textArea').value = ''
-
+        this.counter++;
         if (this.counter !== 1 && this.counter < this.choosenWord.length) {
-          var el = document.getElementById(this.ID)
-          el.remove()
+          var el = document.getElementById(this.ID);
+          el.remove();
         }
-        // if the counter equals the choosen word leangth display win meesage and how many tries
+        // if the counter equals the choosen word leangth display win meesage and how many tries 
         else if (this.counter === this.choosenWord.length) {
-          var el = document.getElementById(this.ID)
-          el.remove()
+          var el = document.getElementById(this.ID);
+          el.remove();
 
           const pRp = document.createElement('p')
-          pRp.style.fontSize = 'large'
-          pRp.className = 'winMessage'
-          pRp.style.fontWeight = 'bold'
-          const ptxts = document.createTextNode('WON!')
+          pRp.style.color = "#000"
+          pRp.style.fontSize = "large";
+          pRp.className = "winMessage"
+          pRp.style.fontWeight = 'bold';
+          const ptxts = document.createTextNode("WON!")
           pRp.appendChild(ptxts)
           myWindow.div.appendChild(pRp)
         }
       }
     }
 
-    // display characters
+    //Show the char and it's position
     if (document.getElementById(this.ID) == null) {
       const pRp = document.createElement('p')
-      pRp.setAttribute('id', this.ID)
-      pRp.style.color = '#000'
-      const ptxts = document.createTextNode(this.underscore)
+      pRp.setAttribute("id", this.ID);
+      pRp.style.color = "#000"
+      const ptxts = document.createTextNode(this.underscoreReplacment)
       pRp.appendChild(ptxts)
       myWindow.div.appendChild(pRp)
     }
   }
 }
+
